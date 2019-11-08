75893564c73bbc7fc75e1975f3a30ed71d4b3574
Lisa Hammarstrand <lisa@Lisas-MacBook-Air.local>
2019-11-08 14:31:41 +0100

    Styled the project

--- script.js
+++ script.js
@@ -19,38 +19,38 @@ const displayRestaurants = (item) => {
     const averageCost = item.restaurant.average_cost_for_two// Try to show dollar signs instead of numbers
     const photo = item.restaurant.thumb
     const rating = item.restaurant.user_rating.rating_text
-   
+
     document.getElementById("restaurants").innerHTML += `<div class="restaurant">  
         <img src="${photo}" alt="${name} photo">
         <h2>${name}</h2>
         <p><span class="rating">Rating: ${rating}</span> <span class="average-cost"> Average cost: ${averageCost}</span></p>
         <p class="address">${address}</p>
         </div>` // Add placeholder img where empty
-       }
+}
 
-let ascendingPrice = false 
+let ascendingPrice = false
 
 const sortByPrice = () => {
-    const priceAscending = "&sort=cost&order=asc" 
+    const priceAscending = "&sort=cost&order=asc"
     const priceDescending = "&sort=cost&order=desc"
-    if (ascendingPrice === false){
-        fetch (url + priceAscending, {headers:{"user-key": apiKey}})
-        .then(res => res.json())
-        .then(json => { 
-            const restaurants = json.restaurants
-            document.getElementById("restaurants").innerHTML = "";
-            restaurants.forEach(displayRestaurants)
-        })  
+    if (ascendingPrice === false) {
+        fetch(url + priceAscending, { headers: { "user-key": apiKey } })
+            .then(res => res.json())
+            .then(json => {
+                const restaurants = json.restaurants
+                document.getElementById("restaurants").innerHTML = "";
+                restaurants.forEach(displayRestaurants)
+            })
         ascendingPrice = true
         sortButton.innerHTML = "Sort by descending price"
     } else {
-        fetch (url + priceDescending, {headers:{"user-key": apiKey}})
-        .then(res => res.json())
-        .then(json => { 
-            const restaurants = json.restaurants
-            document.getElementById("restaurants").innerHTML = "";
-            restaurants.forEach(displayRestaurants)
-        })
+        fetch(url + priceDescending, { headers: { "user-key": apiKey } })
+            .then(res => res.json())
+            .then(json => {
+                const restaurants = json.restaurants
+                document.getElementById("restaurants").innerHTML = "";
+                restaurants.forEach(displayRestaurants)
+            })
         ascendingPrice = false
         sortButton.innerHTML = "Sort by ascending price"
     }
