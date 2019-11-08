8560df468184597b90374304e7be6b9684a9b605
ag223jc <ag223jc@student.lnu.se>
2019-11-08 12:23:56 +0100

    added sort function

--- script.js
+++ script.js
@@ -8,22 +8,56 @@ const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&c
 fetch(url, { headers: { "user-key": apiKey } })
     .then(res => res.json())
     .then(json => {
-        json.restaurants.forEach(resto => {
-            const name = resto.restaurant.name
-            const address = resto.restaurant.location.address
-            const priceSymbol = "$";
-            const priceRange = priceSymbol.repeat(resto.restaurant.price_range) // Try to show dollar signs instead of numbers
-            const photo = resto.restaurant.thumb
-            const rating = resto.restaurant.user_rating.rating_text
-            document.getElementById("restaurants").innerHTML += `<div class="restaurant">  
-            <img src="${photo}" alt="${name} photo">
-            <h2>${name}</h2>
-            <p><span class="rating">Rating: ${rating}</span> <span class="price-range">Price range: ${priceRange}</span></p>
-            <p class="address">${address}</p>
-            </div>` // Add placeholder img where empty
-        })
+        const restaurants = json.restaurants
+
+        restaurants.forEach(displayRestaurants)
     })
 
+const displayRestaurants = (list) => {
+    const name = list.restaurant.name
+    const address = list.restaurant.location.address
+    const averageCost = list.restaurant.average_cost_for_two// Try to show dollar signs instead of numbers
+    const photo = list.restaurant.thumb
+    const rating = list.restaurant.user_rating.rating_text
+   
+    document.getElementById("restaurants").innerHTML += `<div class="restaurant">  
+        <img src="${photo}" alt="${name} photo">
+        <h2>${name}</h2>
+        <p><span class="rating">Rating: ${rating}</span> <span class="average-cost"> Average cost: ${averageCost}</span></p>
+        <p class="address">${address}</p>
+        </div>` // Add placeholder img where empty
+       }
 
 
+let ascendingPrice = false 
 
+const sortByPrice = () => {
+    const priceAscending = "&sort=cost&order=asc" 
+    const priceDescending = "&sort=cost&order=desc"
+    if (ascendingPrice === false){
+        fetch (url + priceAscending, {headers:{"user-key": apiKey}})
+        .then(res => res.json())
+        .then(json => { 
+            const restaurants = json.restaurants
+            document.getElementById("restaurants").innerHTML = "";
+            restaurants.forEach(displayRestaurants)
+        })  
+        ascendingPrice = true
+        sortButton.innerHTML = "Sort by descending price"
+    } else {
+        fetch (url + priceDescending, {headers:{"user-key": apiKey}})
+        .then(res => res.json())
+        .then(json => { 
+            const restaurants = json.restaurants
+            document.getElementById("restaurants").innerHTML = "";
+            restaurants.forEach(displayRestaurants)
+        })
+        ascendingPrice = false
+        sortButton.innerHTML = "Sort by ascending price"
+    }
+}
+
+const sortButton = document.getElementById("sortPrice")
+sortButton.addEventListener("click", () => {
+    sortByPrice()
+})
